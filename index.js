36e811c05132d505d3c28878aecf61ec3b53f1b0
unknown <vins.rendon@gmail.com>
2025-09-18 16:05:41 +0800

    jwt implementation

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express")
 const cors = require("cors")
+const cookieParser = require("cookie-parser")
+
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use(express.json())
+app.use(cookieParser());
+
+
 
 const PORT = 8080;
 
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,34 @@
 const express = require("express")
 const bcrypt = require("bcrypt")
-const crypto = require('crypto')
+// const crypto = require('crypto')
 const router = express.Router()
+const jwt = require("jsonwebtoken")
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+
+const SECRET = process.env.ACCESS_TOKEN_SECRET;
 
 const  { getUsers , registerUser, loginUser } = require('../database.js')
 
 router.get('/users' , async (req,res) => {
-    const users = await getUsers()
-    res.json(users);
+    const token = req.cookies.token;
+
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+    try {
+        const decoded = jwt.verify(token, SECRET);
+        // if(decoded.user)
+        const users = await getUsers()
+        res.status(200).json(users);
+    } 
+    catch (err) {
+        res.status(401).json({ message: "Invalid token" });
+    }
+
+
+    
 })
 
 router.post("/login" , async (req,res) => {
@@ -25,19 +46,22 @@ router.post("/login" , async (req,res) => {
             const isValid = await bcrypt.compare(password,user[0].password)
 
             if(isValid){
-                const generateRandomToken = (length = 64, encoding = 'hex') => {
-                    return crypto.randomBytes(length).toString(encoding);
-                };
+                const token = jwt.sign({ user: user[0].username,role:user[0].role }, SECRET, { expiresIn: "12h" });
 
-                const myToken = generateRandomToken();
+                res.cookie("token", token, {
+                httpOnly: true, 
+                secure: false,    // set to true in production with HTTPS
+                sameSite: "strict",
+                maxAge: 60 * 60 * 12000 // 12 hour
+                });
 
-                return res.json({status:"200",message: "LOGGED IN", token: myToken})
+                return res.status(200).json({message: "LOGGED IN", username:user[0].username, role:user[0].role})
             }
             else
-                return res.json({status:"401", message: "WRONG USER OR PASS"})
+                return res.status(401).json({message: "WRONG USER OR PASS"})
 
         } else {
-            return res.json({status:"404", message: "NO USER FOUND"})
+            return res.status(401).json({message: "NO USER FOUND"})
         }
 
     } catch (error) {
@@ -45,8 +69,16 @@ router.post("/login" , async (req,res) => {
     }
 })
 
+router.post("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.json({ message: "Logged out" });
+});
+
 router.post("/register" , async (req,res) => {
-    const {username,password,role,flag,fname,mname,lname,pnumber,address} = req.body    
+    const token = req.cookies.token;
+    const {username,password,role,flag,fname,mname,lname,pnumber,address} = req.body   
+
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
 
     if (!username || !password || !role || !flag|| !fname || !lname|| !pnumber|| !address) {
         return res.json({status:"201", message: "Fill all necessary fields." })
@@ -62,5 +94,21 @@ router.post("/register" , async (req,res) => {
         console.log(error)
     }
 })
+
+router.get("/verify", (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  try {
+    const decoded = jwt.verify(token, SECRET);
+    // console.log(decoded);
+    
+    res.status(200).json({message: "user verified", user: decoded});
+  } 
+  catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+});
 
 module.exports = router
