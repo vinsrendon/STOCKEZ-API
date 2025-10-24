083f5686d2173f9183242d468c73aa6372fea495
unknown <vins.rendon@gmail.com>
2025-10-24 18:13:52 +0800

    db update

--- stockez.sql
+++ stockez.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 23, 2025 at 01:03 PM
+-- Generation Time: Oct 24, 2025 at 12:12 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
