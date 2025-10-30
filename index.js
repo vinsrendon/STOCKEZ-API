3b9b4612ce0285d8afc1b412fc1c2a6b227a01ea
unknown <vins.rendon@gmail.com>
2025-10-30 11:30:45 +0800

    cors change

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
 const express = require("express")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
+const dotenv = require('dotenv')
 
+dotenv.config()
 
 const app = express()
 
-// app.use(cors({
-//     // origin:'http://localhost:5173',
-//     origin: function (origin, callback) {
-//     // Allow any origin that ends with :5173
-//     if (!origin || origin.match(/^http:\/\/.*:5173$/)) {
-//       callback(null, true);
-//     } 
-    
-//     else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//     credentials:true
-// }))
 app.use(
   cors({
     origin: function (origin, callback) {
-      // Allow localhost:5173 or your Netlify frontend
-      const allowedOrigins = [
-        "http://localhost:5173",
-        "https://stockez-frontend.netlify.app",
-      ];
-
-      if (!origin || allowedOrigins.includes(origin)) {
+      if (!origin || allowedOrigins.includes(process.env.ALLOWED_ORIGINS.split(","))) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
@@ -45,7 +27,7 @@ app.use(cookieParser());
 const PORT = 8080;
 
 app.listen(PORT, ()=>{
-    console.log(`server running on port http://localhost:${PORT}/`);
+    console.log(`SERVER IS RUNNING`);
 });
 
 
