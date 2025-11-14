10378effb9f54a4a9cb3601cada411182c742452
unknown <vins.rendon@gmail.com>
2025-11-15 04:40:19 +0800

    cors issue

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [];
+console.log(allowedOrigins);
 
 const app = express()
 
