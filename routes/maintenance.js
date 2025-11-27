886628515b2a1f269ac4b2d885be1565862c8151
unknown <vins.rendon@gmail.com>
2025-11-27 22:21:03 +0800

    backup endpoint fix

--- routes/maintenance.js
+++ routes/maintenance.js
@@ -5,29 +5,34 @@ const fs = require('fs')
 const path = require('path')
 const BACKUP_DIR = path.join(process.cwd(), 'backups')
 const {verifyToken} = require('./verify.js')
+// const mysql = require('mysql2/promise')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 router.get("/backup" , async (req,res) => {
   const token = req.cookies.token;
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+  if (!token) return res.status(401).json({ message: "Unauthorized" })
   
   if (!fs.existsSync(BACKUP_DIR)) {
-    fs.mkdirSync(BACKUP_DIR);
+    fs.mkdirSync(BACKUP_DIR)
   }
   
   verifyToken(req,res)
 
   const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
-  const backupFile = path.join(BACKUP_DIR, `${process.env.MYSQL_DATABASE}-${timestamp}.sql`);
+  const backupFile = path.join(BACKUP_DIR, `${process.env.MYSQL_DATABASE}-${timestamp}.sql`)
 
-  const dumpCommand = `"C:\\xampp\\mysql\\bin\\mysqldump.exe" -u ${process.env.MYSQL_USER} ${process.env.MYSQL_PASSWORD ? `-p${process.env.MYSQL_PASSWORD}` : ''} --routines ${process.env.MYSQL_DATABASE} > "${backupFile}"`;
+  const dumpCommand = `mysqldump -u ${process.env.MYSQL_USER} ${process.env.MYSQL_PASSWORD ? `-p${process.env.MYSQL_PASSWORD}` : ''} ${process.env.MYSQL_DATABASE} > "${backupFile}"`;
 
   exec(dumpCommand,{ shell: true }, (error, stdout, stderr) => {
     if (error) {
-        console.error('Backup failed:', stderr);
-        return res.status(500).json({ message: 'Backup failed', error: stderr });
+      console.error('Backup failed:', stderr);
+      res.status(500).json({ message: 'Backup failed', error: stderr || error.message});
     }
-
-    res.status(200).json({ message: 'Backup created successfully', file: backupFile });
+    else{
+      res.status(200).json({ message: 'Backup created successfully', file: backupFile });
+    }    
   });
 })
 
